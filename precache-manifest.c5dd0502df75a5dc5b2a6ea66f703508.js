7d58603a30cb22b1a91bcb0c9207ce1b619c7510
michael <625026338@qq.com>
2020-05-08 18:56:14 +0800

    deploy

---
+++ precache-manifest.c5dd0502df75a5dc5b2a6ea66f703508.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc100bc9c3266bde21143125db24f72",
+    "url": "/investment-prediction/index.html"
+  },
+  {
+    "revision": "6ffff9c2dad17bc94d91",
+    "url": "/investment-prediction/static/css/2.aa20a1c3.chunk.css"
+  },
+  {
+    "revision": "138b1d436db8d2915e43",
+    "url": "/investment-prediction/static/css/main.05db3ffd.chunk.css"
+  },
+  {
+    "revision": "6ffff9c2dad17bc94d91",
+    "url": "/investment-prediction/static/js/2.e3cf35de.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/investment-prediction/static/js/2.e3cf35de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "138b1d436db8d2915e43",
+    "url": "/investment-prediction/static/js/main.8db944be.chunk.js"
+  },
+  {
+    "revision": "0778a32f6277313ae79b",
+    "url": "/investment-prediction/static/js/runtime-main.e84b84cc.js"
+  }
+]);
